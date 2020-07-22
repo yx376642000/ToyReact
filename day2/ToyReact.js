471d3b33e3f8b49be8be81de20791f438dd17dc0
yuanxin <yuanxin05791@hellobike.com>
2020-07-22 17:49:56 +0800

    2

---
+++ day2/ToyReact.js
@@ -0,0 +1,130 @@
+class ElementWrapper {
+  constructor(type) {
+      this.root = document.createElement(type);
+  }
+  setAttribute(name, value) {
+      if (name.match(/^on([\s\S]+)$/)) {
+          const eventName = RegExp.$1.replace(/^[\s\S]/, s => s.toLowerCase());
+          this.root.addEventListener(eventName, value);
+      }
+      if (name === "className") {
+          name = this.root.setAttribute("class", value);
+      } else {
+          this.root.setAttribute(name, value);
+      }
+  }
+  appendChild(vchild) {
+      const range = document.createRange();
+      if (this.root.children.length) {
+          range.setStartAfter(this.root.lastChild);
+          range.setEndAfter(this.root.lastChild);
+      } else {
+          range.setStart(this.root, 0);
+          range.setEnd(this.root, 0);
+      }
+      vchild.mountTo(range);
+  }
+  mountTo(range) {
+      range.deleteContents();
+      range.insertNode(this.root);
+  }
+}
+class TextWrapper {
+  constructor(content) {
+      this.root = document.createTextNode(content);
+  }
+  mountTo(range) {
+      range.deleteContents();
+      range.insertNode(this.root);
+  }
+}
+export class Component {
+  constructor () {
+      this.children = [];
+      this.props = Object.create(null);
+  }
+  setAttribute (name, value) {
+      this.props[name] = value;
+      this[name] = value;
+  }
+  mountTo(range) {
+      this.range = range;
+      this.update();
+  }
+  update () {
+      const placeHolder = document.createComment('placeholder');
+      const range = document.createRange();
+      range.setStart(this.range.endContainer, this.range.endOffset);
+      range.setEnd(this.range.endContainer, this.range.endOffset);
+      range.insertNode(placeHolder);
+
+      this.range.deleteContents();
+      const vdom = this.render();
+      vdom.mountTo(this.range);
+
+      // placeHolder.parentNode.removeChild(placeHolder);
+  }
+  appendChild (vchild) {
+      this.children.push(vchild);
+  }
+  setState (state) {
+      const merge = (oldState, newState) => {
+          for (let p in newState) {
+              if (typeof newState[p] === "object") {
+                  if (typeof oldState[p] !== "object") {
+                      oldState[p] = {};
+                  }
+                  merge(oldState[p], newState[p]);
+              } else {
+                  oldState[p] = newState[p];
+              }
+          }
+      }
+      if (!this.state && state) {
+          this.state = {}
+      }
+      merge(this.state, state);
+      this.update();
+  }
+}
+export const ToyReact = {
+  createElement (type, attributes, ...children) {
+      // console.log(type);
+      const element = typeof type === "string"
+          ? new ElementWrapper(type)
+          : new type;
+      for (let name in attributes) {
+          element.setAttribute(name, attributes[name]);
+      }
+      const insertChildren = (children) => {
+          for (let child of children) {
+              if (typeof child === "object" && child instanceof Array) {
+                  insertChildren(child)
+              } else {
+                  if (!(child instanceof Component)
+                      && !(child instanceof ElementWrapper)
+                      && !(child instanceof TextWrapper)
+                  ) {
+                      child = String(child);
+                  }
+                  if (typeof child === "string")
+                      child = new TextWrapper(child);
+                  element.appendChild(child);
+              }
+          }
+      }
+      insertChildren(children);
+      return element;
+  },
+  render (vdom, element) {
+      const range = document.createRange();
+      if (element.children.length) {
+          range.setStartAfter(element.lastChild);
+          range.setEndAfter(element.lastChild);
+      } else {
+          range.setStart(element, 0);
+          range.setEnd(element, 0);
+      }
+      vdom.mountTo(range);
+  }
+};
---
+++ day2/main.js
@@ -0,0 +1,52 @@
+import { ToyReact, Component } from "./ToyReact.js";
+
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+    render() {
+        return (
+            <button className="square" onClick={() => this.setState({ value: 'X'})}>
+                {this.state.value ? this.state.value : ""}
+            </button>
+        );
+    }
+}
+
+class Board extends Component {
+    renderSquare(i) {
+        return (
+            <Square value={i} />
+        );
+    }
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+const a = <Board />
+
+ToyReact.render(
+    a,
+    document.body
+);
